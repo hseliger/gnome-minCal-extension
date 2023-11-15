55faec4120872cc26fca21b52c8c8df811eb2147
Hendrik G. Seliger <github@hseliger.eu>
2023-11-15 09:27:00 +0100

    Updated for Gnome 45

--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as Calendar from 'resource:///org/gnome/shell/ui/calendar.js';
-//import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class MiniCalExtension { //extends Extension {
+export default class MiniCalExtension { 
 
 	enable()
 	{
